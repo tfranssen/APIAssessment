20a0ce96e751f29c961f9d7deab1d8d74d64c8ab
tfranssen <thijsfranssen@gmail.com>
2020-05-14 22:21:18 +0200

    Change build file

--- getTemp/build.sh
+++ getTemp/build.sh
@@ -5,7 +5,5 @@ npm ci
 rm getTemp*.zip
 zip -r getTemp.zip .
 
-aws lambda update-function-code --function-name currentTempInCovilha --zip-file fileb://getTemp.zip --output text &> /dev/null
-
-echo $Version
+aws lambda update-function-code --function-name currentTempInCovilha --zip-file fileb://getTemp.zip --output text 
 
