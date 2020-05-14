af87144b4fa42c3f4a337cc0b7b03ee2f13e79ff
tfranssen <thijsfranssen@gmail.com>
2020-05-14 22:18:16 +0200

    New build file

--- getTemp/build.sh
+++ getTemp/build.sh
@@ -5,8 +5,6 @@ npm ci
 rm getTemp*.zip
 zip -r getTemp.zip .
 
-Version=$(md5 -b CurrentTempFunction.zip | awk '{print $1}')
-
 aws lambda update-function-code --function-name currentTempInCovilha --zip-file fileb://getTemp.zip --output text &> /dev/null
 
 echo $Version
