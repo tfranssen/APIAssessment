d173b881486c4a8aea3893ba0e0eb2a0fa13d079
Thijs Franssen <thijsfranssen@gmail.com>
2020-05-18 10:26:49 +0200

    Change update script

--- scripts/update.sh
+++ scripts/update.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-cd ..
 echo "Current version"
 cat version.env | grep version 
 version=$(cat version.env | grep version | awk -F "=" '{print $2}' | awk -F. -v OFS=. 'NF==1{print ++$NF}; NF>1{if(length($NF+1)>length($NF))$(NF-1)++; $NF=sprintf("%0*d", length($NF), ($NF+1)%(10^length($NF))); print}')
